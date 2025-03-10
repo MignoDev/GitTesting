deade571707817b99f8db70e8ea4475169a244c2
MignoDev <miguelbejaranoing@gmail.com>
2025-03-10 16:09:11 -0500

    Quiz de productos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,86 +7,132 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        RuletaRusa ruletaRusa;
-        Scanner scanner = new Scanner(System.in);
-        List<String>jugadores = new ArrayList<>();
 
-        Random random = new Random();
+        List<Producto> listProductos = new ArrayList<>();
+        Scanner tcl = new Scanner(System.in);
+        int opcion = 1;
 
-        int jugadorTurno;
-        int numeroJugadores;
-        int opcion;
+        while (opcion != 0)
+        {
+            System.out.println("Opción 1: crear producto\nOpción 2: vender producto\nOpción 3: Reponer stock\nOpción 4: Aplicar descuento\nOpción 5: información del producto\nOpción 6: subir precio");
+            System.out.println("Opción 7: valor inventario\nOpción 8: Comparar precios");
+            opcion = tcl.nextInt();
+            switch (opcion){
+                case 1 -> {
+                    System.out.println("Ingrese el nombre del producto");
+                    String nombre = tcl.next();
+                    System.out.println("Ingrese valor del producto");
+                    double precio = tcl.nextDouble();
+                    System.out.println("Ingrese el stock del producto");
+                    int stock = tcl.nextInt();
+                    listProductos.add(new Producto(nombre, precio, stock));
+                }
+                case 2 -> {
+                    System.out.println("Ingrese el nombre del producto que desea vender");
+                    String nombreProducto = tcl.next();
 
-        //region Inicio del juego
-        System.out.println("Ruleta rusa, presione 1 para jugar cualquier otra numero para cerrar el juego");
-        opcion = scanner.nextInt();
-        //endregion
-        while(opcion == 1) {
-
-            //region seleccion de jugadores
-            do {
-                System.out.println("ingrese cantidad de jugadores");
-                numeroJugadores = scanner.nextInt();
-                if (numeroJugadores <= 0) {
-                    System.out.println("numero de jugadores invalido");
-                } else {
-                    for (int i = 0; i < numeroJugadores; i++) {
-                        System.out.println("ingrese nombre del jugador " + (i+1));
-                        jugadores.add(scanner.next());
+                    for(Producto producto : listProductos)
+                    {
+                        if(producto.getNombre().equals(nombreProducto))
+                        {
+                            do{
+                                System.out.println("ingrese la cantidad que se desea vender");
+                            } while(!producto.vender(tcl.nextInt()));
+                            break;
+                        }
                     }
                 }
-            } while (numeroJugadores <= 0);
-            int nCamaras;
-            if (numeroJugadores < 4)
-            {
-                nCamaras = 4;
-            } else if( numeroJugadores > 6)
-            {
-                nCamaras = 6;
-            } else {
-                nCamaras = numeroJugadores;
+                case 3 -> {
+                    System.out.println("Ingrese el nombre del producto que desea reponer");
+                    String nombreProducto = tcl.next();
+                    for(Producto producto : listProductos)
+                    {
+                        if(producto.getNombre().equals(nombreProducto))
+                        {
+                            System.out.println("ingrese la cantidad que se desea reponer");
+                            producto.reponerStock(tcl.nextInt());
+                            System.out.println("Cantidad actual en stock de " + producto.getNombre() + ": " + producto.getStock());
+                            break;
+                        }
+                    }
+                }
+                case 4 -> {
+                    System.out.println("Ingrese el nombre del producto al que desea apliacarle descuento del 2%");
+                    String nombreProducto = tcl.next();
+                    for(Producto producto : listProductos)
+                    {
+                        if(producto.getNombre().equals(nombreProducto))
+                        {
+                            System.out.println("Precio actual: " + producto.getPrecio());
+                            producto.aplicarDescuento();
+                            System.out.println("Precio tras descuento: " + producto.getPrecio());
+                            break;
+                        }
+                    }
+                }
+                case 5 -> {
+                    System.out.println("Ingrese el nombre del producto para ver su información");
+                    String nombreProducto = tcl.next();
+                    for(Producto producto : listProductos)
+                    {
+                        if(producto.getNombre().equals(nombreProducto))
+                        {
+                            System.out.println("información del producto: ");
+                            System.out.println(producto.toString());
+                            break;
+                        }
+                    }
+                }
+                case 6 -> {
+                    System.out.println("Ingrese el nombre del producto al que desea subir el precio");
+                    String nombreProducto = tcl.next();
+                    for(Producto producto : listProductos)
+                    {
+                        if(producto.getNombre().equals(nombreProducto))
+                        {
+                            System.out.println("porcentaje que desea subir el precio");
+                            double porcentaje = tcl.nextDouble();
+                            System.out.println("precio anterior: " + producto.getPrecio());
+                            producto.aumnetarPrecio(porcentaje);
+                            System.out.println("Precio actual tras aumento del " + porcentaje + "%: " + producto.getPrecio());
+                            break;
+                        }
+                    }
+                }
+                case 7 -> {
+                    System.out.println("Ingrese el nombre del producto para ver el valor del inventario");
+                    String nombreProducto = tcl.next();
+                    for(Producto producto : listProductos)
+                    {
+                        if(producto.getNombre().equals(nombreProducto))
+                        {
+                            System.out.println("Valor del niventario de" + producto.getNombre() + ": " + producto.valorInventario());
+                            break;
+                        }
+                    }
+                }
+                case 8 -> {
+                    System.out.println("ingrese el primer producto para comparar");
+                    String producto1 = tcl.next();
+                    System.out.println("ingrese el segundo producto para comparar");
+                    String producto2 = tcl.next();
+
+                    for (Producto productoPrimero : listProductos)
+                    {
+                        if (productoPrimero.getNombre().equals(producto1))
+                        {
+                            for (Producto productoSegundo : listProductos)
+                            {
+                                if (productoSegundo.getNombre().equals(producto2))
+                                {
+                                    productoPrimero.productoMayorQue(productoSegundo);
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            ruletaRusa = new RuletaRusa(nCamaras);
-            //endregion
-
-            //region Desarrollo del juego
-
-            jugadorTurno = random.nextInt(0, jugadores.size() - 1);
-
-            do {
-                System.out.println("turno del jugador " + jugadores.get(jugadorTurno));
-                System.out.println("presione 1 para disparar");
-                opcion = scanner.nextInt();
-                if (opcion == 1)
-                {
-                    if (ruletaRusa.disparar()) {
-                        System.out.println("Jugador " + jugadores.get(jugadorTurno) + " eliminado");
-                        jugadores.remove(jugadorTurno);
-                        if (jugadorTurno >= jugadores.size()-1) {
-                            jugadorTurno = 0;
-                        } else {
-                            jugadorTurno++;
-                        }
-                    } else {
-                        if (jugadorTurno >= jugadores.size()-1) {
-                            jugadorTurno = 0;
-                        } else {
-                            jugadorTurno++;
-                        }
-                    }
-                }
-            } while (jugadores.size() >= 2);
-
-
-            //endregion
-
-            //region fin del juego
-            System.out.println("Ganador jugador " + jugadores.getFirst());
-            System.out.println("Si desea reinciar presione 1, si desea reiniciar,si desea salir presione otro numero");
-            opcion = scanner.nextInt();
-            //endregion
         }
-
 
     }
 }
---
+++ src/main/java/org/example/Producto.java
@@ -0,0 +1,122 @@
+package org.example;
+
+public class Producto {
+    private String nombre;
+
+    private double precio;
+
+    private int stock;
+    private double descuento;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.descuento = 0.02;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                '}';
+    }
+
+    //region Métodos
+    
+    //region VentaProductos
+    public boolean vender(int cantidad)
+    {
+        if (cantidad <= this.stock)
+        {
+            this.stock -= cantidad;
+            return true;
+        }
+        System.out.println("Stock insuficiente, stock actual: " + stock);
+        return false;
+    }
+    //endregion
+
+    //region Reponer stock
+    public void reponerStock (int cantidad)
+    {
+        stock += cantidad;
+    }
+    //endregion
+
+    //region descuento
+    public double aplicarDescuento ()
+    {
+        this.precio -= this.precio * this.descuento;
+        return this.precio;
+    }
+    //endregion
+
+    //region aumentarPrecio
+    public void aumnetarPrecio (double porcentaje)
+    {
+        double porcentajeFinal = porcentaje/100;
+        this.precio += this.precio * porcentajeFinal;
+    }
+    //endregion
+
+    //region valorInventario
+    public double valorInventario ()
+    {
+        return this.stock*this.precio;
+    }
+    //endregion
+
+    public void productoMayorQue( Producto producto)
+    {
+        if(this.precio < producto.getPrecio())
+        {
+            System.out.println("El producto " + producto.getNombre() + " es "+ (producto.getPrecio() - this.precio) + " más costoso");
+            return;
+        }
+
+        if (this.precio > producto.getPrecio())
+        {
+            System.out.println("El producto " + this.nombre + " es " + (this.precio-producto.getPrecio()) +" más costoso");
+            return;
+        }
+
+        System.out.println("Ambos productos tienen el mismo valor");
+
+    }
+    //endregion
+}
